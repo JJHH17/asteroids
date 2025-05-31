9cf16b451dd57fe9139450328b915053c5d06fe7
James Hatfield <james.hatfield17@icloud.com>
2025-05-31 09:20:39 +0100

    Create main file, import constants, add environment config to git ignore

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 # This allows us to use code from the pygame library
 import pygame
+from constants import *
 
 def main():
+    pygame.init() # Initializer
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Creating a new GUI window for game
+
+    while True: # Infinite game loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return # Exit criteria
+            
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
