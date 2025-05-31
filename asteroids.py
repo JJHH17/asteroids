8af23e419d442f86888ac72910b28b79516e97db
James Hatfield <james.hatfield17@icloud.com>
2025-05-31 20:12:44 +0100

    Overrided draw method in asteroid class

---
+++ asteroids.py
@@ -0,0 +1,11 @@
+# File responsible for asteroid class
+
+from circleshape import CircleShape
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
