89f4fec7c87ffacd5caf38778a07c8f7ffe709a9
James Hatfield <james.hatfield17@icloud.com>
2025-05-31 11:34:38 +0100

    Added player instance to main function, draws to screen

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 # This allows us to use code from the pygame library
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init() # Initializer
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Creating a new GUI window for game
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Creating a player instance
     while True: # Infinite game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return # Exit criteria
-            
+
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         # Limiting framerate to 60 FPS
         dt = clock.tick(60) / 1000
