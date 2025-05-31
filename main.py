f2e34d18ad076c5e44e38f8327a807f80d942ca6
James Hatfield <james.hatfield17@icloud.com>
2025-05-31 11:03:22 +0100

    Create object for circle shape objects

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init() # Initializer
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Creating a new GUI window for game
 
     while True: # Infinite game loop
@@ -13,9 +15,9 @@ def main():
             
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        # Limiting framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
