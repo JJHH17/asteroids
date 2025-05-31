041b17a1ace14ecddaaee8101729886972e29434
James Hatfield <james.hatfield17@icloud.com>
2025-05-31 15:42:23 +0100

    Refactored code to use groups

--- main.py
+++ main.py
@@ -9,16 +9,27 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Creating a new GUI window for game
+
+    updatable = pygame.sprite.Group()
+    drawbable = pygame.sprite.Group()
+    Player.containers = (updatable, drawbable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Creating a player instance
+
+
     while True: # Infinite game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return # Exit criteria
 
+        updatable.update(dt)
         pygame.Surface.fill(screen, (0,0,0))
-        player.draw(screen)
-        player.update(dt)
+        
+        for object in drawbable:
+            object.draw(screen)
+
         pygame.display.flip()
+        
         # Limiting framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
